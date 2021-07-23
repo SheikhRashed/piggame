a424087fe6b901280ea7dd24793d6ed68ed925b4
Sheikh Rashed <mdrashed1310@gmail.com>
2021-07-23 16:32:51 +0600

    game restart functionality added

--- script.js
+++ script.js
@@ -15,15 +15,27 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-let playing = true;
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player.forEach(item => item.classList.remove('player--winner'));
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
+// Starting conditions
 
 // Switich player function
 function switchPlayer() {
@@ -81,3 +93,6 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+// Restart game
+btnNew.addEventListener('click', init);
