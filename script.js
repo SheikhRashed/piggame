fa3e3acbd694a377bed601e553e7fdd98a922f9f
Sheikh Rashed <mdrashed1310@gmail.com>
2021-07-23 15:08:00 +0600

    switching player dynamically added

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 'use strict';
 
 // UI Selectors
+const player = document.querySelector('.player');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
@@ -16,7 +19,11 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+
 // Rolling dice functionally
 btnRoll.addEventListener('click', function () {
   // 1. Generate random dice roll
@@ -28,10 +35,16 @@ btnRoll.addEventListener('click', function () {
 
   // 3 . checked for rolled 1
   if (dice !== 1) {
-    // ADd dice to current score
+    // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    // switch to next playerr
+    // switch to next player
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
